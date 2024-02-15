fcfc37c002568bfce94843a805de0731e3f5b39c
jakynevs <johnnevilleis@hotmail.co.uk>
2024-02-15 15:27:39 +0100

    Reorganised files

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import { Header } from "./ui/Header";
-import { Home } from "./ui/Home";
 import styles from './App.module.css';
+import Home from "./ui/Home/Home";
 
 function App() {
     return (
