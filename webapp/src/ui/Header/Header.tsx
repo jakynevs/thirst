ce0c1883f0b4a593132ec34ca9a713854280d40e
jakynevs <johnnevilleis@hotmail.co.uk>
2024-02-22 19:19:56 +0100

    Separated successful order response message into 2 lines

--- webapp/src/ui/Header/Header.tsx
+++ webapp/src/ui/Header/Header.tsx
@@ -1,17 +1,23 @@
 import logo from "./rviewer-logo.svg";
 import styles from './Header.module.css';
+import React from "react";
 
 type HeaderProps = {
     text: string;
   };
   
-  export const Header: React.FC<HeaderProps> = ({ text }) => {
+export const Header: React.FC<HeaderProps> = ({ text }) => {
+    const lines = text.split('\n');
 
     return (
         <header className={styles.header}>
             <img width={80} src={logo} className={styles.rviewerLogo} alt="Rviewer logo"/>
             <h3>
-                {text}
+                {lines.map((line, index) => (
+                    <React.Fragment key={index}>
+                        {line}{index < lines.length - 1 && <br />}
+                    </React.Fragment>
+                ))}
             </h3>
         </header>
     )
