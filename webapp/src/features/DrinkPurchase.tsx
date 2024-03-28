ca7ee160a66c40551fca4b60e9090711e5356886
jakynevs <johnnevilleis@hotmail.co.uk>
2024-03-28 07:59:04 +0100

    Prettier changes

--- webapp/src/features/DrinkPurchase.tsx
+++ webapp/src/features/DrinkPurchase.tsx
@@ -1,76 +1,77 @@
-import { useState } from 'react';
-import Dropdown from '../ui/Components/Dropdown';
-import EnterMoneyField from '../ui/Components/EnterMoneyField';
-import Earnings from './Earnings';
+import { useState } from "react";
+import Dropdown from "../ui/Components/Dropdown";
+import EnterMoneyField from "../ui/Components/EnterMoneyField";
+import Earnings from "./Earnings";
 
 type DrinkPurchaseProps = {
-    onDrinkPurchase: (message: string) => void; 
-  };
+  onDrinkPurchase: (message: string) => void;
+};
 
 function DrinkPurchase({ onDrinkPurchase }: DrinkPurchaseProps) {
-    const [selectedDrink, setSelectedDrink] = useState('');    
-    const [moneyGiven, setMoneyGiven] = useState('');
-    const [showEarnings, setShowEarnings] = useState(false);
-    const [earningsTrigger, setEarningsTrigger] = useState(0);
+  const [selectedDrink, setSelectedDrink] = useState("");
+  const [moneyGiven, setMoneyGiven] = useState("");
+  const [showEarnings, setShowEarnings] = useState(false);
+  const [earningsTrigger, setEarningsTrigger] = useState(0);
 
+  const handleEarningsToggle = () => {
+    setShowEarnings(!showEarnings);
+  };
 
-    const handleEarningsToggle = () => {
-        setShowEarnings(!showEarnings)
-        } 
+  // Function to increment triggerFetch
+  const handleFetchEarnings = () => {
+    setEarningsTrigger((prev) => prev + 1);
+  };
 
-    // Function to increment triggerFetch
-    const handleFetchEarnings = () => {
-        setEarningsTrigger(prev => prev + 1);
-        };
+  const handleBuyClick = async (e) => {
+    e.preventDefault();
+    if (!selectedDrink) {
+      onDrinkPurchase("Please select a drink first.");
+    } else {
+      console.log(`Purchasing ${selectedDrink} with ${moneyGiven}`);
+      try {
+        const response = await fetch("http://localhost:8000/order", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            drinkName: selectedDrink,
+            moneyGiven: moneyGiven,
+          }),
+        });
 
-    const handleBuyClick = async (e) => {
-        e.preventDefault();
-        if (!selectedDrink) {
-            onDrinkPurchase('Please select a drink first.')
+        const result = await response.json();
+        if (response.ok) {
+          console.log("Success", result);
+          onDrinkPurchase(result.message);
+          handleFetchEarnings();
         } else {
-        console.log(`Purchasing ${selectedDrink} with ${moneyGiven}`);
-        try {
-            const response = await fetch('http://localhost:8000/order', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    drinkName: selectedDrink,
-                    moneyGiven: moneyGiven
-                }),
-            })
-            
-            const result = await response.json();
-            if (response.ok) {
-                console.log('Success', result)
-                onDrinkPurchase(result.message)
-                handleFetchEarnings()
-                
-            } else {
-                console.error('HTTP error:', response.status);
-                onDrinkPurchase(result.message)
-                
-            }
-        } catch (error) {
-            console.log('Network error', error)
-            onDrinkPurchase('Failed to complete purchase due to a network error.');
-        }}
-    
-    };
-  
-    return (
-        <div>
-            <Dropdown selectedDrink={selectedDrink} setSelectedDrink={setSelectedDrink} />
-            <EnterMoneyField moneyGiven={moneyGiven} setMoneyGiven={setMoneyGiven} />
-            <button onClick={handleBuyClick}>Buy</button>
-            <button onClick={handleEarningsToggle}>{showEarnings ? 'Hide' : 'Show'} Earnings</button>
-            <div style={{ display: showEarnings ? 'block' : 'none' }}>
-                <Earnings triggerFetch={earningsTrigger} />
-            </div>      
-        </div>
-    );
-  }
+          console.error("HTTP error:", response.status);
+          onDrinkPurchase(result.message);
+        }
+      } catch (error) {
+        console.log("Network error", error);
+        onDrinkPurchase("Failed to complete purchase due to a network error.");
+      }
+    }
+  };
 
-  export default DrinkPurchase;
+  return (
+    <div>
+      <Dropdown
+        selectedDrink={selectedDrink}
+        setSelectedDrink={setSelectedDrink}
+      />
+      <EnterMoneyField moneyGiven={moneyGiven} setMoneyGiven={setMoneyGiven} />
+      <button onClick={handleBuyClick}>Buy</button>
+      <button onClick={handleEarningsToggle}>
+        {showEarnings ? "Hide" : "Show"} Earnings
+      </button>
+      <div style={{ display: showEarnings ? "block" : "none" }}>
+        <Earnings triggerFetch={earningsTrigger} />
+      </div>
+    </div>
+  );
+}
 
+export default DrinkPurchase;
--- webapp/src/features/Earnings.tsx
+++ webapp/src/features/Earnings.tsx
@@ -2,51 +2,54 @@ import React, { useEffect, useState } from "react";
 
 // Add props type for Earnings
 type EarningsProps = {
-    triggerFetch: number;
-  };
-  
-const Earnings: React.FC<EarningsProps> = ({triggerFetch}) => {
-    // Updating the earnings state to have a more specific type
-    const [earnings, setEarnings] = useState<{
-        totalEarnings: number;
-        earningsByDrink: Record<string, number>;
-    } | null>(null);
-    
-    const fetchEarnings = async () => {
-        try {
-            const response = await fetch('http://localhost:8000/earnings')
-            if (!response.ok) {
-                throw new Error('Network reponse was not ok')
-            }
-            const data = await response.json()
-            setEarnings(data)
-        } catch(error) {
-            console.error('Error fetching earnings data', error)
-        }
+  triggerFetch: number;
+};
+
+const Earnings: React.FC<EarningsProps> = ({ triggerFetch }) => {
+  // Updating the earnings state to have a more specific type
+  const [earnings, setEarnings] = useState<{
+    totalEarnings: number;
+    earningsByDrink: Record<string, number>;
+  } | null>(null);
+
+  const fetchEarnings = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/earnings");
+      if (!response.ok) {
+        throw new Error("Network reponse was not ok");
+      }
+      const data = await response.json();
+      setEarnings(data);
+    } catch (error) {
+      console.error("Error fetching earnings data", error);
     }
-    
-    useEffect(() => {
-        console.log("Test")
-        fetchEarnings();
-    }, [triggerFetch])
+  };
 
-    return (
+  useEffect(() => {
+    console.log("Test");
+    fetchEarnings();
+  }, [triggerFetch]);
+
+  return (
     <div>
-        <h3>
-            Earnings: ${earnings ? earnings.totalEarnings.toFixed(2) : "Loading..."}
-        </h3>
-        <div>
-            {earnings ? (
-                <ul>
-                    {Object.entries(earnings.earningsByDrink).map(([drink, amount]) =>
-                    <li key={drink}>{drink}: ${amount.toFixed(2)}</li>)}
-                </ul>
-            ) : (
-                    <p>Loading earnings by drink...</p>
-                )}
-        </div>
+      <h3>
+        Earnings: ${earnings ? earnings.totalEarnings.toFixed(2) : "Loading..."}
+      </h3>
+      <div>
+        {earnings ? (
+          <ul>
+            {Object.entries(earnings.earningsByDrink).map(([drink, amount]) => (
+              <li key={drink}>
+                {drink}: ${amount.toFixed(2)}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>Loading earnings by drink...</p>
+        )}
+      </div>
     </div>
-    )
-}
+  );
+};
 
-export default Earnings 
+export default Earnings;
