0b835c0e6c4d1eafc93db7990e85152bc124841e
jakynevs <johnnevilleis@hotmail.co.uk>
2024-04-02 11:09:53 +0200

    Adjusted for errors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,18 +1,15 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import cors from 'cors';
 import { loadEndpoints } from './controllers/api';
 
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(express.json());
+app.use(bodyParser.json());
 app.use(cors())
 app.set('port', port)
 
 loadEndpoints(app)
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
 
 export default app;
